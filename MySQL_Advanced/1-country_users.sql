a53fd80b548e6caf99cbba649cd54e2cdd3b8d5d
OmerMirzeyev <mirzeyevomer1@gmail.com>
2026-04-18 10:44:16 +0000

    task1

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,7 @@
--- Create a users table with country field
--- if the table doesn't exists
+-- Creates a table 'users' with id, email, name, and country (enum)
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(256) NOT NULL,
-    name VARCHAR(256),
-    country ENUM('US','CO','TN') NOT NULL DEFAULT 'US',
-    PRIMARY KEY (id),
-    UNIQUE (email)
-) ENGINE=InnoDB;
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
